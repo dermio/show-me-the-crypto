4f59fc7badbd40d3327c7b16461795e90973740e
dermio <thisdjay@gmail.com>
2021-06-10 20:43:48 -0700

    Add function to display footer info

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function displayFooterInfo() {
+  let footer = document.getElementsByTagName("footer")[0];
+  let year = `© ${new Date().getFullYear()}`;
+  let link = "<a href='#top'>Show Me the Crypto<a>";
+  footer.innerHTML = `${year} ${link}`;
+}
+
 function startApp() {
   // code
+  displayFooterInfo();
 }
 
 function ready(fn) {
