78c6709507fc61df90ebdaa75619ead03c5dc76d
dermio <thisdjay@gmail.com>
2021-06-22 17:58:53 -0700

    Add fetchCryptoData function

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function fetchCryptoData() {
+  let API_URL = "https://api.coingecko.com/api/v3/coins/markets";
+
+  /* Change the object with the API string parameters into
+  a URLSearchParams object. The URL object has utility methods that work
+  with the query string of a URL. */
+  let params = new URLSearchParams({
+    vs_currency: "USD",
+    per_page: 100
+  });
+
+  // Change the URL parameter object into a URL query string.
+  let queryParams = params.toString();
+
+  fetch(API_URL + "?" + queryParams)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
 function displayFooterInfo() {
   let footer = document.getElementsByTagName("footer")[0];
   let year = `© ${new Date().getFullYear()}`;
@@ -8,6 +27,7 @@ function displayFooterInfo() {
 function startApp() {
   // code
   displayFooterInfo();
+  fetchCryptoData();
 }
 
 function ready(fn) {
